a45169b4d8b531111a765af5faea0f2a77ae29f1
IDGS-803-22001449 <81856@alumnos.utleon.edu.mx>
2025-09-28 22:33:19 -0600

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>horoscopo-react</h1>
+      <h1>Horoscopo React Kevin</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
